b230c14dca1281aa90c436db8c412f52a30c4011
Terje Olsøybakk <terje.olsoybakk@artsdatabanken.no>
2021-01-12 08:58:53 +0100

    Full implementation of v1, v2, v2b, v2.1

---
+++ NinKode.Database/Service/v1/CodeV1Service.cs
@@ -0,0 +1,139 @@
+﻿namespace NinKode.Database.Service.v1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using NinKode.Common.Models.Code;
+    using NinKode.Database.Model.v2;
+    using NinKode.Database.Model.v2b;
+    using Raven.Client.Document;
+    using Raven.Client.Linq;
+
+    public class CodeV1Service
+    {
+        private readonly DocumentStore _store;
+
+        public CodeV1Service(string url, string databaseName)
+        {
+            _store = new DocumentStore
+            {
+                Url = url,
+                DefaultDatabase = databaseName
+            };
+            _store.Initialize(true);
+        }
+
+        public IEnumerable<Codes> GetAll(string host)
+        {
+            using (var session = _store.OpenSession())
+            {
+                var query = session.Query<NaturTypeV2>("NaturTypes/ByKode");
+                using (var enumerator = session.Advanced.Stream(query))
+                {
+                    while (enumerator.MoveNext())
+                    {
+                        yield return CreateCodesByNaturtype(enumerator.Current.Document, host);
+                    }
+                }
+            }
+        }
+
+        public Codes GetByKode(string id, string host)
+        {
+            if (string.IsNullOrEmpty(id)) return null;
+
+            id = id.Replace("_", " ");
+
+            using (var session = _store.OpenSession())
+            {
+                var query = session.Query<NaturTypeV2>("NaturTypes/ByKode").Where(x => x.Kode.Equals(id, StringComparison.OrdinalIgnoreCase));
+                using (var enumerator = session.Advanced.Stream(query))
+                {
+                    while (enumerator.MoveNext())
+                    {
+                        return CreateCodesByNaturtype(enumerator.Current.Document, host);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        #region private methods
+
+        private static Codes CreateCodesByNaturtype(NaturTypeV2 naturType, string host)
+        {
+            if (naturType == null) return null;
+
+            return new Codes
+            {
+                Navn = naturType.Navn,
+                Kategori = naturType.Kategori,
+                Kode = new AllCodesCode
+                {
+                    Id = naturType.Kode,
+                    Definition = $"{host}{naturType.Kode.Replace(" ", "_")}"
+                },
+                ElementKode = naturType.ElementKode,
+                OverordnetKode = new AllCodesCode
+                {
+                    Id = naturType.OverordnetKode,
+                    Definition = !string.IsNullOrEmpty(naturType.OverordnetKode) ? $"{host}{naturType.OverordnetKode.Replace(" ", "_")}" : ""
+                },
+                UnderordnetKoder = naturType.UnderordnetKoder == null ? null : CreateCodesByNaturtype(naturType.UnderordnetKoder, host).ToArray(),
+                //Kartleggingsenheter = naturType.Kartleggingsenheter == null ? null : CreateKartleggingsenheter(naturType.Kartleggingsenheter, host),
+                //Miljovariabler = naturType.Trinn == null ? null : CreateTrinn(naturType.Trinn).ToArray()
+            };
+        }
+
+        private static IEnumerable<AllCodesCode> CreateCodesByNaturtype(string[] koder, string host)
+        {
+            if (koder == null) yield break;
+
+            foreach (var kode in koder)
+            {
+                yield return new AllCodesCode
+                {
+                    Id = kode,
+                    Definition = $"{host}{kode.Replace(" ", "_")}"
+                };
+            }
+        }
+
+        private static Dictionary<string, AllCodesCode[]> CreateKartleggingsenheter(IDictionary<string, string[]> kartleggingsenheter, string host)
+        {
+            if (kartleggingsenheter == null) return null;
+
+            var result = new Dictionary<string, AllCodesCode[]>();
+
+            foreach (var kartlegging in kartleggingsenheter)
+            {
+                result.Add(kartlegging.Key, CreateCodesByNaturtype(kartlegging.Value, host).ToArray());
+            }
+
+            return result;
+        }
+
+        private static IEnumerable<EnvironmentVariable> CreateTrinn(IEnumerable<TrinnV2B> naturTypeTrinn)
+        {
+            return naturTypeTrinn.Select(x => new EnvironmentVariable
+            {
+                Kode = x.Kode,
+                Navn = x.Navn,
+                Type = x.Type,
+                Trinn = x.Trinn == null ? null : CreateTrinn(x.Trinn).ToArray()
+            });
+        }
+
+        private static IEnumerable<Step> CreateTrinn(IEnumerable<SubTrinnV2B> naturTypeTrinn)
+        {
+            return naturTypeTrinn.Select(x => new Step
+            {
+                Kode = x.Kode,
+                Navn = x.Navn
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ NinKode.WebApi/Controllers/v1/CodeV1Controller.cs
@@ -0,0 +1,50 @@
+﻿namespace NinKode.WebApi.Controllers.v1
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    using NinKode.Common.Models.Code;
+    using NinKode.Database.Service.v1;
+
+    [ApiController]
+    [Route("v1")]
+    public class CodeV1Controller : ControllerBase
+    {
+        private const string DatabaseUrl = "http://localhost:8080/";
+        private const string DefaultDatabase = "SOSINiNv1";
+
+        private readonly CodeV1Service _codeService;
+
+        public CodeV1Controller()
+        {
+            _codeService = new CodeV1Service(DatabaseUrl, DefaultDatabase);
+        }
+
+        [HttpGet]
+        [Route("allekoder")]
+        public IEnumerable<Codes> GetAll()
+        {
+            var result = _codeService.GetAll($"{GetHostPath()}hentkode/");
+            foreach (var code in result)
+            {
+                yield return code;
+            }
+        }
+
+        [HttpGet]
+        [Route("hentkode/{id}")]
+        public Codes GetCode(string id)
+        {
+            return _codeService.GetByKode(id, GetHostPath());
+        }
+
+        private string GetHostPath()
+        {
+            var path = Request.Path.Value;
+            path = path.Substring(0, path.LastIndexOf("/", StringComparison.Ordinal) + 1);
+            var protocol = Request.IsHttps ? "s" : "";
+            return $"http{protocol}://{Request.Host}{path}";
+        }
+    }
+}
