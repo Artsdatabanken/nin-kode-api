b3e74cc19f53b7f764a9f5cd3f6988e6a352442d
Terje Olsøybakk <terje.olsoybakk@artsdatabanken.no>
2021-01-15 10:24:32 +0100

    Miljøvariabel-tekst from metadata.json

---
+++ NinKode.Database/Model/common/LkmKoder.cs
@@ -0,0 +1,15 @@
+﻿namespace NinKode.Database.Model.common
+{
+    using System;
+
+    public class LkmKoder
+    {
+        public int index { get; set; }
+        public string kode { get; set; }
+
+        public string trinnkode => string.IsNullOrEmpty(kode)
+            ? ""
+            : kode.Substring(kode.LastIndexOf("-", StringComparison.Ordinal) + 1);
+        public LkmTittel tittel { get; set; }
+    }
+}
--- NinKode.Database/Model/v22/SubTrinnV22.cs
+++ NinKode.Database/Model/v22/SubTrinnV22.cs
@@ -4,5 +4,6 @@
     {
         public string Navn { get; set; }
         public string Kode { get; set; }
+        public string Basistrinn { get; set; }
     }
 }
