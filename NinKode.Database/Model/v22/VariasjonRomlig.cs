70cc825d5dab901b3fd8b281f7b555ad96683fb7
Terje Olsøybakk <terje.olsoybakk@artsdatabanken.no>
2021-01-18 17:26:17 +0100

    Romlig - ok

---
+++ NinKode.Database/Model/v22/VariasjonRomlig.cs
@@ -0,0 +1,49 @@
+﻿namespace NinKode.Database.Model.v22
+{
+    using System;
+    using System.Text.Json.Serialization;
+    using Raven.Imports.Newtonsoft.Json;
+
+    public class VariasjonRomlig
+    {
+        [JsonProperty("docId")]
+        [JsonPropertyName("docId")]
+        public string docId { get; set; }
+        [JsonProperty("Kode")]
+        [JsonPropertyName("Kode")]
+        public string Kode { get; set; }
+        [JsonProperty("Vaiabelgruppe")]
+        [JsonPropertyName("Vaiabelgruppe")]
+        public string Vaiabelgruppe { get; set; }
+        [JsonProperty("Kode2")]
+        [JsonPropertyName("Kode2")]
+        public string Kode2 { get; set; }
+        [JsonProperty("Navn")]
+        [JsonPropertyName("Navn")]
+        public string Navn { get; set; }
+        [JsonProperty("trinn")]
+        [JsonPropertyName("trinn")]
+        public string Trinn { get; set; }
+        [JsonProperty("Sammensatt_kode")]
+        [JsonPropertyName("Sammensatt_kode")]
+        public string SammensattKode { get; set; }
+        [JsonProperty("Forklaring")]
+        [JsonPropertyName("Forklaring")]
+        public string Forklaring { get; set; }
+        [JsonProperty("Måleintervall")]
+        [JsonPropertyName("Måleintervall")]
+        public string Maaleintervall { get; set; }
+        [JsonProperty("Variabeltype")]
+        [JsonPropertyName("Variabeltype")]
+        public string VariabelType { get; set; }
+        [JsonProperty("Tags")]
+        [JsonPropertyName("Tags")]
+        public string Tags { get; set; }
+        [JsonProperty("DTG_Timestamp")]
+        [JsonPropertyName("DTG_Timestamp")]
+        public DateTime DtgTimestamp { get; set; }
+        [JsonProperty("DTG_Ticks")]
+        [JsonPropertyName("DTG_Ticks")]
+        public long DtgTicks { get; set; }
+    }
+}
