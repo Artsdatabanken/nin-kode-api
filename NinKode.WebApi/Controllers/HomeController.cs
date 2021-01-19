e38ebeabb79db99f57b23a43f3d0370c276182ad
Terje Olsøybakk <terje.olsoybakk@artsdatabanken.no>
2021-01-19 15:01:33 +0100

    Hide HomeController from Swagger

---
+++ NinKode.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿namespace NinKode.WebApi.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return Redirect("../swagger/index.html");
+        }
+    }
+}
