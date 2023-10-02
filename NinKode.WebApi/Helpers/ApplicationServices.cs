9e6ddf8dc0cf520f76c8503836fb1f4432cc1ada
Eirik <eirik.niemi@artsdatabanken.no>
2023-10-02 08:54:48 +0200

    Adjust startup

--- NinKode.WebApi/Helpers/ApplicationServices.cs
+++ NinKode.WebApi/Helpers/ApplicationServices.cs
@@ -1,35 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
-using NiN.Database;
-using NinKode.Common.Interfaces;
+﻿using NinKode.Common.Interfaces;
 using NinKode.Database.Services;
-using NinKode.WebApi.Helpers.Swagger;
 
 namespace NinKode.WebApi.Helpers;
 
 public static class ApplicationServices
 {
-    public static void AddApplicationServices(this IServiceCollection services, ConfigurationManager configuration)
+    public static void AddApplicationServices(this IServiceCollection services)
     {
-        // database
-        var connectionString = configuration.GetConnectionString("Default");
-        if (string.IsNullOrEmpty(connectionString))
-            throw new Exception("Could not find 'Default' ConnectionString");
-        services.AddDbContext<NiNDbContext>(o =>
-        {
-            o.UseSqlServer(connectionString, x => x.MigrationsAssembly(typeof(NiNDbContext).Module.Name));
-        });
-
         services.AddSingleton<ICodeService, CodeService>();
         services.AddSingleton<IVarietyService, VarietyService>();
         services.AddSingleton<IExportService, ExportService>();
         services.AddSingleton<IImportService, ImportService>();
         services.AddSingleton<IVersionService, VersionService>();
-
-        // API versioning https://github.com/dotnet/aspnet-api-versioning
-        services.ConfigureVersioning();
-
-        // Swagger
-        services.AddSwagger();
-        services.ConfigureOptions<SwaggerOptions>();
     }
 }
--- NinKode.WebApi/Startup.cs
+++ NinKode.WebApi/Startup.cs
@@ -1,4 +1,6 @@
 using Hellang.Middleware.ProblemDetails;
+using Microsoft.EntityFrameworkCore;
+using NiN.Database;
 using NinKode.WebApi.Helpers;
 using NinKode.WebApi.Helpers.Swagger;
 
@@ -6,7 +8,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddApplicationServices(builder.Configuration);
+// ef core database
+var connectionString = builder.Configuration.GetConnectionString("Default");
+if (string.IsNullOrEmpty(connectionString))
+    throw new Exception("Could not find ConnectionString 'Default'");
+
+builder.Services.AddDbContext<NiNDbContext>(o => { o.UseSqlServer(connectionString, x => x.MigrationsAssembly(typeof(NiNDbContext).Module.Name)); });
+
+// misc project services
+builder.Services.AddApplicationServices();
+
+// api versioning
+builder.Services.ConfigureVersioning();
+
+// swagger
+builder.Services.AddSwagger();
+builder.Services.ConfigureOptions<SwaggerOptions>();
 
 builder.Services.AddProblemDetails(options => { options.IncludeExceptionDetails = (_, _) => builder.Environment.IsDevelopment(); });
 
