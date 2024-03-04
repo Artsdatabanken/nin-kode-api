1b5f1419aa0ddccb5510d5f71f25c93c6ec0969e
ADB\sto <stig.thorseth@artdatabanken.no>
2024-03-04 13:37:44 +0100

    #45: nin2+nin3: rapport-controller working

---
+++ NinKode.WebApi/Controllers/3_0/SøkController.cs
@@ -0,0 +1,31 @@
+﻿/*
+using Microsoft.AspNetCore.Mvc;
+using NiN3.Core.Models.Enums;
+using NiN3.Infrastructure.Services;
+
+namespace NiN3.WebApi.Controllers
+{
+    [ApiVersion("3.0")]
+    [ApiController]
+    public class SøkController : Controller
+    {
+    private readonly ISearchService _searchService;
+
+        public SøkController(ISearchService searchService)
+        {
+            _searchService = searchService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("enkeltSøk")]
+        public IActionResult SimpleSearch(string searchTerm, KlasseEnum klasseEnum, SearchMethodEnum searchMethodEnum)
+        {
+            var results = _searchService.SimpleSearch(searchTerm, klasseEnum, searchMethodEnum);
+            return Ok(results);
+        }
+    }
+}
+*/
--- NinKode.WebApi/Startup.cs
+++ NinKode.WebApi/Startup.cs
@@ -1,6 +1,8 @@
 using Hellang.Middleware.ProblemDetails;
 using Microsoft.EntityFrameworkCore;
 using NiN.Database;
+using NiN3.Infrastructure.DbContexts;
+using NiN3.Infrastructure.Services;
 using NinKode.WebApi.Helpers;
 using NinKode.WebApi.Helpers.Swagger;
 
@@ -10,10 +12,18 @@ builder.Services.AddControllers();
 
 // ef core database
 var connectionString = builder.Configuration.GetConnectionString("Default");
+var nin3ConnectionString = builder.Configuration.GetConnectionString("NiN3");
 if (string.IsNullOrEmpty(connectionString))
     throw new Exception("Could not find ConnectionString 'Default'");
+if (string.IsNullOrEmpty(nin3ConnectionString))
+    throw new Exception("Could not find ConnectionString 'NiN3'");
 
 builder.Services.AddDbContext<NiNDbContext>(o => { o.UseSqlite(connectionString); });
+builder.Services.AddDbContext<NiN3DbContext>(o => { o.UseSqlite(nin3ConnectionString); });
+
+// Adding services
+builder.Services.AddScoped<IRapportService, RapportService>();
+builder.Services.AddScoped<ISearchService, SearchService>();
 
 // misc project services
 builder.Services.AddApplicationServices();
