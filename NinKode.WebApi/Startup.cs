14f2b012fe1174f2b5fa4e6ab36fa25e24faf41a
DipShre <dipak.shrestha@artsdatabanken.no>
2025-10-20 09:54:02 +0200

    config close #156

--- NinKode.WebApi/Startup.cs
+++ NinKode.WebApi/Startup.cs
@@ -25,7 +25,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 if (!builder.Environment.IsDevelopment())
 {
-    builder.Services.AddApplicationInsightsTelemetry();
+    builder.Services.ConfigureApplicationInsights();
 }
 
 builder.Services.AddControllers().AddJsonOptions(options =>
