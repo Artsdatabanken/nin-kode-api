7b95bce3a3390ad1b29dd84a7090f6fa5bc4bf2e
DipShre <dipak.shrestha@artsdatabanken.no>
2025-11-03 15:40:40 +0100

    oppdatert telemetry close #156

--- NinKode.WebApi/Startup.cs
+++ NinKode.WebApi/Startup.cs
@@ -25,7 +25,7 @@ var builder = WebApplication.CreateBuilder(args);
 if (!builder.Environment.IsDevelopment())
 {
     builder.Services.AddApplicationInsightsTelemetry();    
-    builder.Services.AddApplicationInsightsTelemetryProcessor<NotFoundTelemetryProcessor>();
+    builder.Services.AddSingleton<ITelemetryInitializer, TelemetryInitializer>();
 }
 
 builder.Services.AddControllers().AddJsonOptions(options =>
